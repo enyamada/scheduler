0d30b354d3b83d3b5cc7b39b70482f9cb765b002
Edson Yamada <enyamada@gmail.com>
2016-04-04 13:55:17 -0300

    db.py and config.py PEP8'ed

--- config.py
+++ config.py
@@ -1,42 +1,79 @@
+
+"""
+  Module that contains functions related to reading and handling
+  configuration files. The yaml module is leveraged here.
+
+"""
+
 import os
 import yaml
 
 
-
-
 def read_aws_env_config(config):
+    """
+    Check if any AWS configuration parameter was specified using a
+    environment variable. If so, it must override the value provided
+    by the dictionary passed (that must be updated for later use).
 
-    config["ami-id"]        = os.environ.get("AWS_AMI_ID", config["ami-id"])
-    config["spot-price"]    = os.environ.get("AWS_SPOT_PRICE", config["spot-price"])
-    config["instance-type"] = os.environ.get("AWS_INSTANCE_TYPE", config["instance-type"])
-    config["key-name"]      = os.environ.get("AWS_KEY_NAME", config["key-name"])
-    config["sg-name"]       = os.environ.get("AWS_SG_NAME", config["sg-name"])
+    Args:
+        config: dictionary containing the aws default parameters
+    """
+
+    config["ami-id"] = os.environ.get("AWS_AMI_ID", config["ami-id"])
+    config[
+        "spot-price"] = os.environ.get("AWS_SPOT_PRICE", config["spot-price"])
+    config[
+        "instance-type"] = os.environ.get("AWS_INSTANCE_TYPE",
+                                          config["instance-type"])
+    config["key-name"] = os.environ.get("AWS_KEY_NAME", config["key-name"])
+    config["sg-name"] = os.environ.get("AWS_SG_NAME", config["sg-name"])
 
 
 def read_app_env_config(config):
+    """
+    Check if any app configuration parameter was specified using a
+    environment variable. If so, it must override the value provided
+    by the dictionary passed (that must be updated for later use).
 
-    config["polling-interval"] = os.environ.get("APP_POLLING_INTERVAL", config["polling-interval"])
+    Args:
+        config: dictionary containing the app default parameters
+    """
 
 
+    config["polling-interval"] = os.environ.get(
+        "APP_POLLING_INTERVAL", config["polling-interval"])
 
-def read_db_env_config (config):
 
+def read_db_env_config(config):
+    """
+    Check if any db configuration parameter was specified using a
+    environment variable. If so, it must override the value provided
+    by the dictionary passed (that must be updated for later use).
+
+    Args:
+        config: dictionary containing the db default parameters
+    """
     config["host"] = os.environ.get("MYSQL_PORT_3306_TCP_ADDR", config["host"])
 
 
-def read_log_env_config (config):
+def read_log_env_config(config):
+    """
+    Check if any log configuration parameter was specified using a
+    environment variable. If so, it must override the value provided
+    by the dictionary passed (that must be updated for later use).
 
+    Args:
+        config: dictionary containing the log default parameters
+    """
     config["level"] = os.environ.get("LOG_LEVEL", config["level"])
 
 
 def read_config(config_file):
-
     """
-
     Reads the parameters described in the passed config file (that must be
-    a YAML file). Then checks if any of them were overriden by a environment 
+    a YAML file). Then checks if any of them were overriden by a environment
     variable and  finally returns a dictionary containing the
-    setting in effect.  
+    setting in effect.
 
     Args:
         config_file: YAML file to be used as a configuration file.
@@ -44,7 +81,6 @@ def read_config(config_file):
     Returns:
         A dictionary as retuned by the yaml module.
     """
-
 
     with open(config_file, "r") as f:
         config = yaml.load(f)
@@ -55,5 +91,3 @@ def read_config(config_file):
     read_log_env_config(config["log"])
 
     return config
-
-
--- db.py
+++ db.py
@@ -4,11 +4,6 @@ Module containing some functions that handle the database.
 
 """
 
-from time import strptime, strftime
-from datetime import datetime
-
-import os
-import time
 
 import logging
 import logging.handlers
@@ -16,28 +11,26 @@ import logging.handlers
 import MySQLdb
 
 
+def update_db(db_conn, job_id, **kwargs):
+    """
 
-
-
-def update_db (db, job_id, **kwargs):
-    """ 
-
-    Updates the information stored in the database (jobs table) regarding
+    Updates the information stored in the database(jobs table) regarding
      a specific job.
 
     Args:
-        db: A database connection (as returned by the open_connection function)
+        db_conn: A database connection(as returned by the open_connection
+            function)
         job_id: The job id
         **kwargs: Here you can specify the columns/values to be updated.
 
     Example:
          update_db(db_conn, 10, callback="http://my-callback.com") updates the
-             database (using the db_conn connection) regarding the job whose 
+             database(using the db_conn connection) regarding the job whose
              id is 10 setting the callback column to ="http://my-callback.com"
 
     """
 
-    cursor = db.cursor()
+    cursor = db_conn.cursor()
 
     set_clause = ""
     for k in kwargs:
@@ -46,35 +39,34 @@ def update_db (db, job_id, **kwargs):
     set_clause = set_clause[:-1]
 
     sql = "UPDATE jobs SET %s WHERE id=%s" % (set_clause, job_id)
-    logging.debug("Update db: %s" % sql)
+    logging.debug("Update db: %s", sql)
     cursor.execute(sql)
 
 
-
-def job_db_data (db, job_id, *cols):
+def job_db_data(db_conn, job_id, *cols):
     """
 
     Reads information stored in the database about a specific job and
     returns as a list.
 
     Args:
-        db: A database connection (as returned by the open_connection function)
+        db_conn: A database connection(as returned by the open_connection
+            function)
         job_id: The job id
         *cols: List of columns to be returned
-   
+
     Returns:
-        A list of columns (as specified by the arguments)
+        A list of columns(as specified by the arguments)
 
     Example:
-         job_db_data (db_conn, 10, "status", "req_state") reads the
-             database (using the db_conn connection) regarding the job whose
+         job_db_data(db_conn, 10, "status", "req_state") reads the
+             database(using the db_conn connection) regarding the job whose
              id is 10 and returns as a list the values found in the "status"
              and "req_state" columns.
 
     """
 
-
-    cursor = db.cursor(MySQLdb.cursors.DictCursor)
+    cursor = db_conn.cursor(MySQLdb.cursors.DictCursor)
     cursor.execute("SELECT * FROM jobs WHERE id=%s" % job_id)
     row = cursor.fetchone()
 
@@ -93,19 +85,16 @@ def open_connection(config):
     Args:
         config: A dictionary containing the database connection parameters.
             Should contain the host, user, secret and db keys specifying
-            the host where the database server is, the user/password to 
+            the host where the database server is, the user/password to
             be used and the database name to log into.
 
     Returns:
-        A db connection (to be used on other db calls)
+        A db connection(to be used on other db calls)
 
 
     """
 
-
-    conn = MySQLdb.connect(config["host"], config["user"], config["secret"], config["db"])
+    conn = MySQLdb.connect(config["host"], config["user"], config
+                           ["secret"], config["db"])
     conn.autocommit(True)
     return conn
-
-
-
