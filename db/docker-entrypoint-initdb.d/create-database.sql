9c47f232dfbf93e72d3216208c42ba5ba0735cc3
Edson Yamada <enyamada@gmail.com>
2016-04-06 02:08:58 +0000

    Adding files for scheduler-db container

---
+++ db/docker-entrypoint-initdb.d/create-database.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 5.5.46, for Linux (x86_64)
+--
+-- Host: localhost    Database: scheduler
+-- ------------------------------------------------------
+-- Server version	5.5.46
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `scheduler`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `scheduler` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `scheduler`;
+
+--
+-- Table structure for table `jobs`
+--
+
+DROP TABLE IF EXISTS `jobs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `jobs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `run_at` datetime NOT NULL,
+  `status` varchar(15) DEFAULT NULL,
+  `env_vars` varchar(500) DEFAULT NULL,
+  `docker_image` varchar(30) NOT NULL,
+  `callback` varchar(255) DEFAULT NULL,
+  `instance_id` varchar(16) DEFAULT NULL,
+  `req_id` varchar(16) DEFAULT NULL,
+  `req_state` varchar(16) DEFAULT NULL,
+  `req_status_code` varchar(128) DEFAULT NULL,
+  `notes` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-04-02 20:23:03
---
+++ db/docker-entrypoint-initdb.d/create-user.sql
@@ -0,0 +1,2 @@
+create user 'scheduler'@'%' identified by "Pass&&wOrd";
+grant all privileges on scheduler.* to 'scheduler'@'%';
