6366a4a2cb0a46eb2efeca68638ddd3fc613017b
Edson Yamada <enyamada@gmail.com>
2016-04-17 22:16:38 -0400

    Using scheduler-web tag 1.2

--- launcher/my-init.sh
+++ launcher/my-init.sh
@@ -11,6 +11,6 @@ yum install docker -y
 service docker start
 docker run --name scheduler-db  -v /var/lib/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=yagg27 -d enyamada/scheduler-db:1.0
 sleep 60
-docker run --name scheduler-web  -e LOG_LEVEL=debug  -v /var/log/:/var/log/ -e AWS_ACCESS_KEY_ID='%s' -e AWS_SECRET_ACCESS_KEY='%s' -d -p 80:80 --link scheduler-db:mysql enyamada/scheduler-web:1.1
+docker run --name scheduler-web  -e LOG_LEVEL=debug  -v /var/log/:/var/log/ -e AWS_ACCESS_KEY_ID='%s' -e AWS_SECRET_ACCESS_KEY='%s' -d -p 80:80 --link scheduler-db:mysql enyamada/scheduler-web:1.2
 touch /tmp/fin
 
